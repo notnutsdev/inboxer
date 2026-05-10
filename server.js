a5ca862f7b5a98660044087aef7b608e8861316c
notnutsdev <notttnuts@gmail.com>
2026-05-11 01:48:08 +0200

    finished staff login and deletion system

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const session = require("express-session");
 const app = express();
 
 const { DatabaseSync } = require("node:sqlite");
@@ -16,6 +17,7 @@ const banned_words = require("./banned_words.json");
 
 const { Webhook, MessageBuilder } = require("discord-webhook-node");
 
+
 // Loading environment variables
 process.loadEnvFile("./.env");
 
@@ -26,6 +28,11 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.static("public"));
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
 
 app.get("/", (req, res) => {
     const posts = db.prepare("SELECT * FROM posts ORDER BY RANDOM() LIMIT 5;").all();
@@ -69,10 +76,14 @@ app.post("/create", (req, res) => {
 
 app.get('/post/:uid', (req, res) => {
     const uid = req.params.uid;
-    const post = db.prepare(`SELECT * FROM posts WHERE uid = '${uid}';`).all()[0];
+    let post = db.prepare(`SELECT * FROM posts WHERE uid = '${uid}';`).all()[0];
     
     if (!post) {
         return res.render("view.ejs", { error: "No post found for this ID." });
+    }
+
+    if (req.session.staff_login) {
+        post.isStaff = true;
     }
 
     res.render("view.ejs", post);
@@ -132,9 +143,76 @@ for (const [key, value] of Object.entries(config.urls)) {
 }
 
 // Staff login URL.
+// Checks if the user isn't already logged in as a staff member
+app.all(process.env.STAFF_LOGIN_URL, (req, res, next) => {
+    if (req.session.staff_login) {
+        return res.redirect("/");
+    };
+    next();
+});
+
 app.get(process.env.STAFF_LOGIN_URL, (req, res) => {
-    res.send("hello admin!");
-    // finish this. Maybe use express-sessions
+    res.render('staff_login.ejs');
+})
+
+app.post(process.env.STAFF_LOGIN_URL, (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (!username || !password) return res.render("staff_login.ejs", { error: "Please fill out a username and password." });
+
+    const staff_codes = {};
+    process.env.STAFF_CODES.split(",").forEach(value => {
+        const user_pass = value.split(':');
+        staff_codes[user_pass[0]] = user_pass[1]
+    }); // Create an object with: { username: password } from the .env data
+
+    // Checks is the username/password provided by the user are in the staff_codes object
+    for (let [key, value] of Object.entries(staff_codes)) {
+        if (username === key && password === value) {
+            req.session.staff_login = { username: username };
+            return res.render("staff_login.ejs", { success: "Login successfull!" });
+        }
+    }
+
+    res.render("staff_login.ejs", { error: "Invalid username/password." });
+})
+
+app.get(process.env.STAFF_LOGIN_URL + "/logout", (req, res) => {
+    if (!req.session.staff_login) {
+        return res.redirect(process.env.STAFF_LOGIN_URL);
+    }
+    req.session.staff_login = null; // Logout the user
+    res.redirect("/");
+})
+
+app.get("/delete/:uid", (req, res) => {
+    const uid = req.params.uid;
+
+    if (!req.session.staff_login) {
+        return res.redirect("/");
+    }
+    
+    const post = db.prepare(`SELECT * FROM posts WHERE uid = '${uid}'`).all();
+
+    if (!post) return req.redirect("/");
+
+    db.exec(`DELETE FROM posts WHERE uid = '${uid}'`);
+
+    // Log everything to the webhook log channel
+    const hook = new Webhook(process.env.STAFF_LOGS_WEBHOOK);
+    const embed = new MessageBuilder()
+    .setAuthor(`Action taken by: ${req.session.staff_login.username}`)
+    .setTitle("Post deleted")
+    .setColor("#fff")
+    .addField("Post ID", `\`${uid}\``)
+    .addField("Action", "Deletion")
+    .addField("Post Author", `\`${post[0].username}\``)
+    .addField("Original content", `\`\`\`${post[0].content}\`\`\``)
+    .setTimestamp();
+    hook.send(embed);
+
+    return res.render("blank.ejs", { success: "Deleted post with ID: " + uid });
 })
 
 app.listen(8000, () => {
