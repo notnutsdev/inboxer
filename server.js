0df4f14aefbf90a9f37081ce164e61d071297c78
notnutsdev <notttnuts@gmail.com>
2026-05-29 09:29:33 +0200

    wokred on user profiles

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ process.loadEnvFile("./.env");
 
 // Routers
 const authRoutes = require("./routes/auth");
+const { where } = require("sequelize");
 
 // Setting ejs as our view engine
 app.set("view engine", "ejs");
@@ -131,6 +132,58 @@ app.get('/post/:uid', async (req, res) => {
     delete post.dataValues.user.dataValues.password;
 
     res.render("view.ejs", { post: post.dataValues, author: post.dataValues.user.dataValues, user: req.session.user });
+});
+
+// User profile page
+app.get("/user/:username", async (req, res) => {
+    const username = req.params.username;
+    const page = req.query.page || 1;
+    const posts_per_page = 10; // The number of posts to display per page
+
+    if (isNaN(page) && isNaN(parseFloat(page))) {
+        return res.render("blank.ejs", { error: "Page must be a number." });
+    }
+
+    if (+page <= 0) {
+        return res.render("blank.ejs", { error: "Page must be greater then 0." });
+    }
+
+    const user = await User.findOne({
+        where: {
+            username: username
+        }
+    });
+
+    if (!user) {
+        return res.render("blank.ejs", { error: "User not found." });
+    }
+
+    // Getting the user's posts
+    const posts = await Post.findAll({
+        where: {
+            user_id: user.uid
+        },
+        offset: (page-1)*10,
+        limit: posts_per_page
+    });
+
+    // Getting the total post count
+    const total_post_count = await Post.count({
+        where: {
+            user_id: user.uid
+        }
+    });
+
+    // Total number of pages from that specific user
+    const total_pages = Math.ceil(total_post_count/posts_per_page);
+
+    if (page > total_pages) {
+        return res.render("blank.ejs", { error: "The profivded page number doesn't exist for this user." });
+    }
+
+    delete user.dataValues.password;
+
+    res.render("profile.ejs", { user: user, posts: posts, page: +page, total_pages: total_pages });
 })
 
 app.get("/random", async (req, res) => {
